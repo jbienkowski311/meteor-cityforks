0fe6a76a912e083ea95a0ad30fe557cab3d42ab0
Jakub Bieńkowski <jbienkowski311@gmail.com>
2016-02-23 10:48:55 +0100

    Leaflet download due to CDN downtime and EJSON fix in order to fetch nearby locations

--- collections/places.js
+++ collections/places.js
@@ -3,9 +3,10 @@ Places = new Mongo.Collection('places');
 Meteor.methods({
 	fetchNearbyLocations: function(coords){
 		if(Meteor.isServer){
-			req = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coords.latitude + "," + coords.longitude + "&radius=500&types=food&key=AIzaSyDyasCmfXtahLpo7F131VbgQZGiNDAN2I8");
+			req = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coords.latitude + "," + coords.longitude + "&radius=500&types=food&key=AIzaSyAd0jS5Bwxp3k7A6GjkYPn_pOTDayLSM_w");
 			_(req.data.results).each(function(place){
 				_.extend(place, {loc: {type: "Point", coordinates: [place.geometry.location.lng, place.geometry.location.lat]}});
+				console.log(place);
 				Places.upsert({googleId: place.id}, {$set: place});
 			});
 		}
---
+++ server/index.js
@@ -0,0 +1,1 @@
+Places._ensureIndex({'loc.coordintes':'2dsphere'});
