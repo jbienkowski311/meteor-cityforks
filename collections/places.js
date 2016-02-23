8a8494d2bbcf73edc28d7bef82083b1bedf1415b
Jakub Bieńkowski <jbienkowski311@gmail.com>
2016-02-23 11:02:44 +0100

    Add index and publications

--- collections/places.js
+++ collections/places.js
@@ -6,7 +6,6 @@ Meteor.methods({
 			req = HTTP.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + coords.latitude + "," + coords.longitude + "&radius=500&types=food&key=AIzaSyAd0jS5Bwxp3k7A6GjkYPn_pOTDayLSM_w");
 			_(req.data.results).each(function(place){
 				_.extend(place, {loc: {type: "Point", coordinates: [place.geometry.location.lng, place.geometry.location.lat]}});
-				console.log(place);
 				Places.upsert({googleId: place.id}, {$set: place});
 			});
 		}
