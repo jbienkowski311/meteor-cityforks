c5060b254326cd8c0cee912920f32f60b2d4ee59
Jakub Bieńkowski <jbienkowski311@gmail.com>
2016-02-24 09:13:53 +0100

    Map flashing (rerendering) fixed, now with markers being added on the fly

--- client/views/home.js
+++ client/views/home.js
@@ -1,33 +1,63 @@
+Template.home.onCreated(function(){
+	L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+	this.mapRendered = false;
+	this.bottomLeft = new ReactiveVar;
+	this.topRight = new ReactiveVar;
+	this.markers = {};
+	this.setBounds = function(bounds){
+		if(!bounds){
+			var bounds = this.map.getBounds();
+		}
+		if(bounds){
+			this.bottomLeft.set([bounds._southWest.lng, bounds._southWest.lat]);
+      		this.topRight.set([bounds._northEast.lng, bounds._northEast.lat]);
+		}
+	};
+	var template = this;
+	template.autorun(function(){
+		template.subscribe('nearbyPlaces', template.bottomLeft.get(), template.topRight.get());
+	});
+});
+
 Template.home.onRendered(function(){
-	Tracker.autorun(function(){
-		if(Session.get('location')){
+	var template = this;
+	template.autorun(function(){
+		if(Session.get('location') && Session.get('location').latitude){
 			var latitude, longitude;
 			latitude = Session.get('location').latitude;
 			longitude = Session.get('location').longitude;
-			var map = L.map('map').setView([latitude, longitude], 16);
+
+		}
+		if(!template.mapRendered){
+			template.map = L.map('map').setView([latitude, longitude], 15);
+			template.mapRendered = true;
 			L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	         	attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-	        }).addTo(map);
-	        L.marker([latitude, longitude]).addTo(map).bindPopup('<h3>Your location</h3>').openPopup();
-	        var bounds = map.getBounds();
-	        if(bounds){
-	        	Session.set('bottomLeft', [bounds._southWest.lng, bounds._southWest.lat]);
-	        	Session.set('topRight', [bounds._northEast.lng, bounds._northEast.lat]);
-	        }
-	     	if(Template.instance()){
-	     		Template.instance().data.forEach(function(place){
-	     			L.marker([place.geometry.location.lat, place.geometry.location.lng]).addTo(map)
-	     				.bindPopup("<strong>" + place.name + "</strong><br />" + place.vicinity);
-	     		});
-	     	}
-	     	map.on('moveend', function(event){
-	     		var bounds = event.target.getBounds();
-	     		Session.set('bottomLeft', [bounds._southWest.lng, bounds._southWest.lat]);
-	        	Session.set('topRight', [bounds._northEast.lng, bounds._northEast.lat]);
-	        	coords = {latitude: event.target.getCenter().lat, longitude: event.target.getCenter().lng};
-	        	Session.set('location', coords);
-	        	Meteor.call('fetchNearbyLocations', coords);
-	     	});
+	        }).addTo(template.map);
+	        template.setBounds();
+	        template.map.on('moveend', function(event) {
+				bounds = event.target.getBounds();
+				template.setBounds(bounds);
+				coords = {latitude: event.target.getCenter().lat, longitude: event.target.getCenter().lng};
+				Meteor.call('fetchNearbyLocations', coords);
+			});
+		}
+	});
+});
+
+Template.home.onRendered(function(){
+	var template = this;
+	Places.find().observeChanges({
+		'added': function(id, place) {
+			if (!template.markers[id]) {
+			    template.markers[id] = L.marker([place.geometry.location.lat, place.geometry.location.lng])
+			    template.map.addLayer(template.markers[id]);
+			    template.markers[id].bindPopup(Blaze.toHTMLWithData(Template.marker, place));
+			}
+		},
+		'removed': function(id) {
+			template.map.removeLayer(template.markers[id]);
+			template.markers[id] = undefined;
 		}
 	});
 });
